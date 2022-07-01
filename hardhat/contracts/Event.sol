a697488400ecfb0bf50d626223c2de1d66a77959
hiroaki terayama <35390466+terapasta@users.noreply.github.com>
2022-07-02 00:21:03 +0900

    create struct EventRecord

--- hardhat/contracts/Event.sol
+++ hardhat/contracts/Event.sol
@@ -10,12 +10,24 @@ contract EventManager {
         string name;
     }
 
+    struct EventRecord {
+        uint256 eventId;
+        uint256 groupId;
+        string name;
+        string description;
+        string date;
+        string startTime;
+        string endTime;
+        bytes32 secretPhrase;
+    }
+
     using Counters for Counters.Counter;
 
     Counters.Counter private _eventIds;
     Counters.Counter private _groupIds;
 
     Group[] public groups;
+    EventRecord[] public eventRecords;
 
     constructor() {
         console.log("init");
@@ -29,10 +41,46 @@ contract EventManager {
 
     function getGroups() public view returns (Group[] memory) {
         uint256 _numberOfGroups = groups.length;
-        console.log("number of groups: %s", _numberOfGroups);
         // create array of groups
         Group[] memory _groups = new Group[](_numberOfGroups);
         _groups = groups;
         return _groups;
+    }
+
+    function createEventRecord(
+        uint256 _groupId,
+        string memory _name,
+        string memory _description,
+        string memory _date,
+        string memory _startTime,
+        string memory _endTime,
+        string memory _secretPhrase
+    ) external {
+        uint256 _newEventId = _eventIds.current();
+        bytes memory hexSecretPhrase = bytes(_secretPhrase);
+        bytes32 encryptedSecretPhrase = keccak256(hexSecretPhrase);
+        eventRecords.push(
+            EventRecord({
+                eventId: _newEventId,
+                groupId: _groupId,
+                name: _name,
+                description: _description,
+                date: _date,
+                startTime: _startTime,
+                endTime: _endTime,
+                secretPhrase: encryptedSecretPhrase
+            })
+        );
+        _eventIds.increment();
+    }
+
+    function getEventRecords() public view returns (EventRecord[] memory) {
+        uint256 _numberOfEventRecords = eventRecords.length;
+        // create array of events
+        EventRecord[] memory _eventRecords = new EventRecord[](
+            _numberOfEventRecords
+        );
+        _eventRecords = eventRecords;
+        return _eventRecords;
     }
 }
