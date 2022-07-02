f387c7901c445769bc4852c3dde6885bd3406aef
Hal Seki <hal@plants-web.jp>
2022-07-02 22:43:34 +0900

    format by prettier

--- frontend/src/pages/events/new.tsx
+++ frontend/src/pages/events/new.tsx
@@ -2,44 +2,43 @@ import { Heading, List, ListItem } from "@chakra-ui/react";
 import { useAddress } from "@thirdweb-dev/react";
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
-import { getEventManagerContract, IEventGroup } from "../../helpers/eventManager";
+import {
+  getEventManagerContract,
+  IEventGroup,
+} from "../../helpers/eventManager";
 
 const EventCreate: NextPage = () => {
-  const address = useAddress()
-  const [groups, setGroups] = useState([])
-  const getOwnEventGroups = async() => {
-    console.log('get my event groups')
-    const eventManager = getEventManagerContract()
-    console.log(eventManager)
-    if (!eventManager) throw 'error'
-    const data = await eventManager.getOwnGroups()
-    console.log(data)
-    setGroups(data)
-  }
+  const address = useAddress();
+  const [groups, setGroups] = useState([]);
+  const getOwnEventGroups = async () => {
+    console.log("get my event groups");
+    const eventManager = getEventManagerContract();
+    console.log(eventManager);
+    if (!eventManager) throw "error";
+    const data = await eventManager.getOwnGroups();
+    console.log(data);
+    setGroups(data);
+  };
   useEffect(() => {
-    if (address){
-      getOwnEventGroups()
+    if (address) {
+      getOwnEventGroups();
     }
-  }, [address])
+  }, [address]);
 
   return (
     <>
-      <Heading>
-        Create a new event
-      </Heading>
+      <Heading>Create a new event</Heading>
       {address ? (
         <List>
-          { groups.map((item: IEventGroup) => {
-            return (
-              <ListItem key={item.groupId}>{item.name}</ListItem>
-            )
-          }) }
+          {groups.map((item: IEventGroup) => {
+            return <ListItem key={item.groupId}>{item.name}</ListItem>;
+          })}
         </List>
-        ) : (
+      ) : (
         <span>please login first</span>
       )}
-    </>      
-  )
-}
+    </>
+  );
+};
 
 export default EventCreate;
