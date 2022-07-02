c7c62fa64d7792223b575bffafad52f7984be7aa
Hal Seki <hal@plants-web.jp>
2022-07-02 23:37:52 +0900

    add react-hook-form

--- frontend/src/pages/events/new.tsx
+++ frontend/src/pages/events/new.tsx
@@ -1,13 +1,30 @@
-import { Heading, List, ListItem } from "@chakra-ui/react";
+import { Heading, List, ListItem, Switch } from "@chakra-ui/react";
 import { useAddress } from "@thirdweb-dev/react";
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Select,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 import {
   getEventManagerContract,
   IEventGroup,
 } from "../../helpers/eventManager";
 
 const EventCreate: NextPage = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "all",
+  });
+  const [groupIdSelcted, setGroupIdSelected] = useState(false);
   const address = useAddress();
   const [groups, setGroups] = useState([]);
   const getOwnEventGroups = async () => {
@@ -24,16 +41,63 @@ const EventCreate: NextPage = () => {
       getOwnEventGroups();
     }
   }, [address]);
-
+  const onSubmit = (data: any) => console.log("onSubmit:", data);
+  const selectGroup = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value) {
+      setGroupIdSelected(true);
+    }
+  };
   return (
     <>
       <Heading>Create a new event</Heading>
       {address ? (
-        <List>
-          {groups.map((item: IEventGroup) => {
-            return <ListItem key={item.groupId}>{item.name}</ListItem>;
-          })}
-        </List>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <FormLabel htmlFor="eventGroupId">Event group: </FormLabel>
+            <Select
+              id="eventGroupId"
+              placeholder="Please select event group"
+              {...register("eventGroupId", {
+                required: "This is required",
+              })}
+              onChange={selectGroup}
+            >
+              {groups.map((item: IEventGroup) => {
+                return (
+                  <option value={item.groupId} key={item.groupId}>
+                    {item.name}
+                  </option>
+                );
+              })}
+            </Select>
+          </FormControl>
+          {groupIdSelcted ? (
+            <>
+              <FormControl>
+                <FormLabel htmlFor="name">Event Name</FormLabel>
+                <Input
+                  id="name"
+                  {...register("name", {
+                    required: "This is required",
+                    minLength: {
+                      value: 4,
+                      message: "Minimum length should be 4",
+                    },
+                  })}
+                ></Input>
+              </FormControl>
+              <Button
+                type="submit"
+                //disabled={!errors?.name}
+                isLoading={isSubmitting}
+              >
+                Create
+              </Button>{" "}
+            </>
+          ) : (
+            <span>please select event group</span>
+          )}
+        </form>
       ) : (
         <span>please login first</span>
       )}
