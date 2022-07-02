dad5f92e6298723609c7d45485b5273ca12063eb
Hal Seki <hal@plants-web.jp>
2022-07-02 12:51:44 +0900

    close #2 add metamask login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,10 +3,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout';
+import { useAddress } from '@thirdweb-dev/react';
+import { Box } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
+  const address = useAddress();
+
   return (
-    <Layout>hoge</Layout>
+    <Layout>
+      
+      hoge
+      <Box>{address}</Box>  
+    </Layout>
   )
 }
 
