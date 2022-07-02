84c179619280f2cd34a6c969554b1da1f26670bb
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-07-02 09:17:11 +0900

    tmp

--- hardhat/contracts/MintNFT.sol
+++ hardhat/contracts/MintNFT.sol
@@ -2,25 +2,89 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MintNFT {
-  // string private greeting;
+contract MintNFT is ERC721Enumerable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  // constructor(string memory _greeting) {
-  //     console.log("Deploying a Greeter with greeting:", _greeting);
-  //     greeting = _greeting;
-  // }
+    struct ParticipateNFTAttributes {
+        string name;
+        string groupId;
+        string eventId;
+    }
 
-  // function greet() public view returns (string memory) {
-  //     return greeting;
-  // }
+    ParticipateNFTAttributes[] participateNFTs;
 
-  // function setGreeting(string memory _greeting) public {
-  //     console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-  //     greeting = _greeting;
-  // }
+    mapping(uint256 => ParticipateNFTAttributes) public attributesOfNFT;
 
-  constructor() {
-    console.log("ok");
-  }
+    constructor() ERC721("MintRally", "MR") {
+        console.log("ok");
+    }
+
+    function mintParticipateNFT(string memory eventId) external {
+        for (uint256 index = 0; index < participateNFTs.length; index++) {
+            ParticipateNFTAttributes memory p = participateNFTs[index];
+            if (keccak256(bytes(p.eventId)) == keccak256(bytes(eventId))) {
+                attributesOfNFT[_tokenIds.current()] = p;
+                _safeMint(msg.sender, _tokenIds.current());
+                _tokenIds.increment();
+                break;
+            }
+        }
+    }
+
+    function checkOwnedNFTsId()
+        external
+        view
+        returns (ParticipateNFTAttributes[] memory)
+    {
+        uint256 tokenCount = balanceOf(msg.sender);
+        uint256[] memory tokenIds = new uint256[](tokenCount);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex((msg.sender), i);
+        }
+        ParticipateNFTAttributes[] memory attributes = getTokens(
+            tokenIds,
+            msg.sender
+        );
+        return attributes;
+    }
+
+    function getTokens(uint256[] memory tokenIds, address _owner)
+        internal
+        view
+        returns (ParticipateNFTAttributes[] memory)
+    {
+        uint256 holdingTokenCount = balanceOf(_owner);
+        ParticipateNFTAttributes[]
+            memory holdingNFTsAttributes = new ParticipateNFTAttributes[](
+                holdingTokenCount
+            );
+        for (uint256 i = 0; i < holdingTokenCount; i++) {
+            uint256 id = tokenIds[i];
+            holdingNFTsAttributes[i] = attributesOfNFT[id];
+        }
+        return holdingNFTsAttributes;
+    }
+
+    function pushParticipateNFT(ParticipateNFTAttributes[] memory attributes)
+        external
+        returns (uint256)
+    {
+        for (uint256 index = 0; index < attributes.length; index++) {
+            participateNFTs.push(
+                ParticipateNFTAttributes({
+                    name: attributes[index].name,
+                    groupId: attributes[index].groupId,
+                    eventId: attributes[index].eventId
+                })
+            );
+        }
+        console.log(participateNFTs[0].name);
+        console.log(participateNFTs[1].name);
+        return _tokenIds.current();
+    }
 }
--- hardhat/test/MintNFT.ts
+++ hardhat/test/MintNFT.ts
@@ -2,8 +2,35 @@ import { ethers } from "hardhat";
 
 describe("MintNFT", function () {
   it("MintNFT test", async function () {
+    const [owner1, owner2] = await ethers.getSigners();
+
     const MintNFT = await ethers.getContractFactory("MintNFT");
-    const greeter = await MintNFT.deploy();
-    await greeter.deployed();
+    const mintNFT = await MintNFT.deploy();
+    await mintNFT.deployed();
+
+    let txn = await mintNFT.pushParticipateNFT([
+      {
+        name: "normalNFT",
+        groupAddress: "0xbbb",
+        eventAddress: "0xaaa",
+      },
+      {
+        name: "specialNFT",
+        groupAddress: "0xbbb",
+        eventAddress: "0xaab",
+      },
+    ]);
+    await txn.wait();
+
+    txn = await mintNFT.connect(owner1).mintParticipateNFT("0xaaa");
+    txn = await mintNFT.connect(owner1).mintParticipateNFT("0xaab");
+    await txn.wait();
+    txn = await mintNFT.connect(owner2).mintParticipateNFT("0xaaa");
+    await txn.wait();
+
+    let currentNum = await mintNFT.connect(owner1).checkOwnedNFTsId();
+    let currentNum2 = await mintNFT.connect(owner2).checkOwnedNFTsId();
+
+    console.log(currentNum, currentNum2);
   });
 });
