1898139503d9f3a226f8da6a5d8ad3f87c4e28c4
Hal Seki <hal@plants-web.jp>
2022-07-02 12:32:33 +0900

    add navbar and footer

---
+++ frontend/src/components/layout.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import Head from 'next/head';
+import Navbar from './navbar'
+import Footer from './footer'
+
+type Props = {
+  children: ReactNode;
+  title?: string;
+};
+const Layout = ({
+  children
+}: Props) => (
+  <>
+    <Head>
+      <title>Mint Rally</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Navbar />
+    <main>{children}</main>
+    <Footer />
+  </>
+)
+export default Layout{}
