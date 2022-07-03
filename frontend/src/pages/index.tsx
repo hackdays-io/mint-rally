66e0a40abf7842a1dde64d197e92e23414789f08
hiroaki terayama <35390466+terapasta@users.noreply.github.com>
2022-07-03 11:38:26 +0900

    implement top page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from "next";
 import { useAddress } from "@thirdweb-dev/react";
 import { Box } from "@chakra-ui/react";
-
+import Image
+  from "next/image";
 const Home: NextPage = () => {
   const address = useAddress();
   return (
     <>
-      hoge
-      <Box>{address}</Box>
+      <div>
+        {/* <div
+          style={{
+            width: '100vw',
+            height: '960px',
+            maxHeight: '500px',
+            backgroundImage: 'url("/images/mainImg.png")',
+            backgroundRepeat: "no-repeat, no-repeat, no-repeat",
+            backgroundSize: "auto, cover, cover",
+            backgroundPosition: "left, center, center"
+          }}
+        /> */}
+        <Image
+          src="/images/mainImg.png"
+          width={1920}
+          height={960}
+          alt="mainImg"
+        />
+      </div>
     </>
   );
 };
