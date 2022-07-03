5f6202d35e728c3aa4a4ee32e1459ea07089287c
Hal Seki <hal@plants-web.jp>
2022-07-03 14:36:29 +0900

    fix event form

--- frontend/src/pages/events/[eventid].tsx
+++ frontend/src/pages/events/[eventid].tsx
@@ -9,6 +9,9 @@ import {
   AlertTitle,
   AlertDescription,
   Input,
+  Container,
+  Flex,
+  Link,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
@@ -92,79 +95,102 @@ const Event = () => {
 
   return (
     <>
-      <Heading>Event details</Heading>
-      {loadingFetch && <Spinner></Spinner>}
-      {event && (
-        <Box>
-          <Text>Event name: {event.name}</Text>
-          <Text>Event description: {event.description}</Text>
-          <Box mt={8} textAlign="center">
-            {!showMintButton ? (
-              <>
-                <Button
-                  isLoading={applyLoading}
-                  onClick={async () => {
-                    await applyForParticipation({
-                      eventId: event.eventRecordId,
-                    });
-                  }}
-                >
-                  Apply
-                </Button>
-                {applyErrors && (
-                  <Alert status="error" mt={2} mx={4}>
-                    <AlertIcon />
-                    <AlertTitle>Error occurred</AlertTitle>
-                    <AlertDescription>{applyErrors.message}</AlertDescription>
-                  </Alert>
-                )}
-                {applyStatus && (
-                  <Alert status="success" mt={2} mx={4}>
-                    <AlertIcon />
-                    <AlertTitle>You have applied!</AlertTitle>
-                  </Alert>
-                )}
-              </>
-            ) : (
-              <>
-                <Text>Enter secret phrase:</Text>
-                <Input
-                  variant="outline"
-                  type="password"
-                  mb={4}
-                  value={enteredSecretPhrase}
-                  onChange={(e) => setEnteredSecretPhrase(e.target.value)}
-                />
-                <Button
-                  isLoading={mintLoading}
-                  onClick={async () => {
-                    await mintParticipateNFT({
-                      groupId: event.groupId,
-                      eventId: (event.eventRecordId as any).toNumber(),
-                      secretPhrase: enteredSecretPhrase,
-                    });
-                  }}
-                >
-                  Claim NFT!
-                </Button>
-                {mintErrors && (
-                  <Alert status="error" mt={2} mx={4}>
-                    <AlertIcon />
-                    <AlertTitle>Error occurred</AlertTitle>
-                    <AlertDescription>{mintErrors.message}</AlertDescription>
-                  </Alert>
-                )}
-                {mintStatus && (
-                  <Alert status="success" mt={2} mx={4}>
-                    <AlertIcon />
-                    <AlertTitle>You have claimed NFT!</AlertTitle>
-                  </Alert>
-                )}
-              </>
-            )}
+      <Container maxW={800} paddingTop={6}>
+        <Heading>Event details</Heading>
+        {loadingFetch && <Spinner></Spinner>}
+        {event && (
+          <Box padding={5}>
+            <Link href={"event-groups/" + event.groupId.toString()}>
+              Event Group ID: {event.groupId.toString()}
+            </Link>
+            <Flex>
+              <Box w={200} fontWeight="bolder">
+                Event name:
+              </Box>
+              <Box>{event.name}</Box>
+            </Flex>
+            <Flex>
+              <Box w={200} fontWeight="bolder">
+                Event description:
+              </Box>
+              <Box>{event.description}</Box>
+            </Flex>
+            <Flex>
+              <Box w={200} fontWeight="bolder">
+                Event date and time:
+              </Box>
+              <Box>
+                {event.date + " " + event.startTime + " - " + event.endTime}
+              </Box>
+            </Flex>
+            <Box mt={8} textAlign="center">
+              {!showMintButton ? (
+                <>
+                  <Button
+                    isLoading={applyLoading}
+                    onClick={async () => {
+                      await applyForParticipation({
+                        eventId: event.eventRecordId,
+                      });
+                    }}
+                  >
+                    Apply
+                  </Button>
+                  {applyErrors && (
+                    <Alert status="error" mt={2} mx={4}>
+                      <AlertIcon />
+                      <AlertTitle>Error occurred</AlertTitle>
+                      <AlertDescription>{applyErrors.message}</AlertDescription>
+                    </Alert>
+                  )}
+                  {applyStatus && (
+                    <Alert status="success" mt={2} mx={4}>
+                      <AlertIcon />
+                      <AlertTitle>You have applied!</AlertTitle>
+                    </Alert>
+                  )}
+                </>
+              ) : (
+                <>
+                  <Text>Enter secret phrase:</Text>
+                  <Input
+                    variant="outline"
+                    type="password"
+                    mb={4}
+                    value={enteredSecretPhrase}
+                    onChange={(e) => setEnteredSecretPhrase(e.target.value)}
+                  />
+                  <Button
+                    isLoading={mintLoading}
+                    onClick={async () => {
+                      await mintParticipateNFT({
+                        groupId: event.groupId,
+                        eventId: (event.eventRecordId as any).toNumber(),
+                        secretPhrase: enteredSecretPhrase,
+                      });
+                    }}
+                  >
+                    Claim NFT!
+                  </Button>
+                  {mintErrors && (
+                    <Alert status="error" mt={2} mx={4}>
+                      <AlertIcon />
+                      <AlertTitle>Error occurred</AlertTitle>
+                      <AlertDescription>{mintErrors.message}</AlertDescription>
+                    </Alert>
+                  )}
+                  {mintStatus && (
+                    <Alert status="success" mt={2} mx={4}>
+                      <AlertIcon />
+                      <AlertTitle>You have claimed NFT!</AlertTitle>
+                    </Alert>
+                  )}
+                </>
+              )}
+            </Box>
           </Box>
-        </Box>
-      )}
+        )}
+      </Container>
     </>
   );
 };
