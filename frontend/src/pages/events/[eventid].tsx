8b74b69e17435f2a276cb8c01d501f72c60d54ac
MaySoMusician <maysomusician@gmail.com>
2022-07-03 12:15:23 +0900

    Use getEventByid instead of getting all events

--- frontend/src/pages/events/[eventid].tsx
+++ frontend/src/pages/events/[eventid].tsx
@@ -1,7 +1,5 @@
 import {
   Heading,
-  List,
-  ListItem,
   Spinner,
   Box,
   Text,
@@ -14,20 +12,22 @@ import {
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import {
-  IEventRecord,
-  useEventRecords,
+  useGetEventById,
   useApplyForParticipation,
 } from "../../hooks/useEventManager";
 
 const Event = () => {
-  const { records, loading, getEventRecords } = useEventRecords();
+  const router = useRouter();
+  const { eventid } = router.query;
+  const { event, loading: loadingFetch, getEventById } = useGetEventById();
   useEffect(() => {
-    getEventRecords();
+    if (eventid) {
+      const id = Array.isArray(eventid) ? eventid[0] : eventid;
+      getEventById({ eventId: Number.parseInt(id, 10) });
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const router = useRouter();
-  const { eventid } = router.query;
   const {
     errors: applyErrors,
     loading: applyLoading,
@@ -37,42 +37,32 @@ const Event = () => {
   return (
     <>
       <Heading>Event details</Heading>
-      {loading && <Spinner></Spinner>}
-      <List>
-        {records
-          .filter((item) => item.eventRecordId.toString() == eventid)
-          .map((item) => {
-            return (
-              <>
-                <ListItem key={"name-" + item.eventRecordId}>
-                  {item.name}
-                </ListItem>
-                <ListItem key={"description-" + item.eventRecordId}>
-                  {item.description}
-                </ListItem>
-                <Box mt={8} textAlign="center">
-                  <Button
-                    isLoading={applyLoading}
-                    onClick={() => {
-                      applyForParticipation({
-                        evendId: item.eventRecordId,
-                      });
-                    }}
-                  >
-                    Register!
-                  </Button>
-                  {applyErrors && (
-                    <Alert status="error" mt={2}>
-                      <AlertIcon />
-                      <AlertTitle>Error occurred</AlertTitle>
-                      <AlertDescription>{applyErrors.message}</AlertDescription>
-                    </Alert>
-                  )}
-                </Box>
-              </>
-            );
-          })}
-      </List>
+      {loadingFetch && <Spinner></Spinner>}
+      {event && (
+        <Box>
+          <Text>Event name: {event.name}</Text>
+          <Text>Event description: {event.description}</Text>
+          <Box mt={8} textAlign="center">
+            <Button
+              isLoading={applyLoading}
+              onClick={() => {
+                applyForParticipation({
+                  evendId: event.eventRecordId,
+                });
+              }}
+            >
+              Apply
+            </Button>
+            {applyErrors && (
+              <Alert status="error" mt={2}>
+                <AlertIcon />
+                <AlertTitle>Error occurred</AlertTitle>
+                <AlertDescription>{applyErrors.message}</AlertDescription>
+              </Alert>
+            )}
+          </Box>
+        </Box>
+      )}
     </>
   );
 };
