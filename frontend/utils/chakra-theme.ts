acaa56ad4903d3e14f6af092dc13fbf58814f983
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-07-19 21:39:40 +0900

    simplify header dom

---
+++ frontend/utils/chakra-theme.ts
@@ -0,0 +1,32 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  mint: {
+    bg: "#56F0DE",
+    primary: "#552306",
+    subtle1: "#A25020",
+    subtle2: "#C68762",
+    white: "#FFFFFF",
+    subtle: "#E3FFFC",
+  },
+};
+
+export const chakraTheme = extendTheme({
+  // https://chakra-ui.com/docs/theming/theme#typography
+  fonts: {},
+  // デフォルトのカラーモード
+  // https://chakra-ui.com/docs/theming/theme#config
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+  colors: colors,
+  // デフォルトのフォント
+  styles: {
+    global: {
+      body: {
+        color: colors.mint.primary,
+      },
+    },
+  },
+});
