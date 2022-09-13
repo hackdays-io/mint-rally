ecc296ca87fc26762e78201f1d83e77d4aa60742
Hal Seki <hal@plants-web.jp>
2022-09-13 22:40:58 +0900

    add locales

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'ja',
+  },
 }
 
 module.exports = nextConfig
