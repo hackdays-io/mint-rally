96a4c69793efd4d9dbd96b38e05d114ed6112fe6
Hal Seki <hal@plants-web.jp>
2022-09-14 00:22:27 +0900

    important pages are localized so far.

--- frontend/src/locales/ja.ts
+++ frontend/src/locales/ja.ts
@@ -4,6 +4,18 @@ export default {
   EVENTS: "イベント",
   SIGN_IN: "ログイン",
   SIGN_OUT: "ログアウト",
-  SWITCH_NETWORK: "ネットワーク切替"
+  SWITCH_NETWORK: "ネットワーク切替",
+  // Event group index
+  CREATE_NEW_EVENT_GROUP: "イベントグループを作成",
+  // Event group page
+  OWN_EVENTS: "のイベント",
+  // Event index
+  CREATE_NEW_EVENT: "イベントを作成",
+  // Event page
+  SIGN_IN_TO_GET_NFT: "NFTを取得するにはログインしてください",
+  YOU_ALREADY_HAVE_THIS_NFT: "既にこのNFTは取得済みです。ご参加ありがとうございました！",
+  ENTER_SECRET_PHRASE: "イベント主催者から聞いたシークレットフレーズを入力してください。",
+  CLAIM_NFT: "NFT取得！",
+  YOU_HAVE_CLAIMED_NFT: "NFTを入手しました！！！"
   // ...
 }
