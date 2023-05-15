43475ba6ab4f9f6709653d0a4dfe55e4b030d6ab
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-05-15 23:30:05 +0900

    tmp

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -12,17 +12,24 @@ import { chakraTheme } from "../../utils/chakra-theme";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { Localhost, Mumbai, Polygon } from "@thirdweb-dev/chains";
 
 config.autoAddCss = false;
 
-const activeChainId = +process.env.NEXT_PUBLIC_CHAIN_ID!;
+const chainId = process.env.NEXT_PUBLIC_CHAIN_ID!;
+const activeChain =
+  chainId === "80001"
+    ? Mumbai
+    : chainId === "137"
+    ? Polygon
+    : { ...Localhost, chainId: 31337 };
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <ThirdwebProvider
-        activeChain={activeChainId}
+        activeChain={activeChain}
         supportedWallets={[
           metamaskWallet(),
           safeWallet(),
