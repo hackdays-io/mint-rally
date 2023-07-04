97df669a7e33ee2aa2b03746f2f73afbaa77acb6
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-07-04 21:51:20 +0900

    edcsa signature verifier

--- hardhat/circuits/SecretPhrase.circom
+++ hardhat/circuits/SecretPhrase.circom
@@ -1,13 +1,47 @@
-pragma circom  2.0.0;
+pragma circom 2.1.2;
+
+include "./secp256k1/mul.circom";
+include "./address/address.circom";
+include "../node_modules/circomlib/circuits/bitify.circom";
 
 template VerifySecretPhrase () {
-    // Declaration of signals.  
-    signal input a;  
-    signal input b;  
-    signal output c;  
+    var bits = 256;
+    signal input s;
+    signal input Tx; // T = r^-1 * R
+    signal input Ty; 
+    signal input Ux; // U = -(m * r^-1 * G)
+    signal input Uy;
+    signal output addr;
 
-    // Constraints.  
-    c <== a * b;
+    // sMultT = s * T
+    component sMultT = Secp256k1Mul();
+    sMultT.scalar <== s;
+    sMultT.xP <== Tx;
+    sMultT.yP <== Ty;
+
+    // pubKey = sMultT + U 
+    component pubKey = Secp256k1AddComplete();
+    pubKey.xP <== sMultT.outX;
+    pubKey.yP <== sMultT.outY;
+    pubKey.xQ <== Ux;
+    pubKey.yQ <== Uy;
+
+
+    component pubKeyXBits = Num2Bits(256);
+    pubKeyXBits.in <== pubKey.outX;
+
+    component pubKeyYBits = Num2Bits(256);
+    pubKeyYBits.in <== pubKey.outY;
+
+    component pubToAddr = PubkeyToAddress();
+
+    for (var i = 0; i < 256; i++) {
+        pubToAddr.pubkeyBits[i] <== pubKeyYBits.out[i];
+        pubToAddr.pubkeyBits[i + 256] <== pubKeyXBits.out[i];
+    }
+
+    addr <== pubToAddr.address;
+    
 }
 
 component main = VerifySecretPhrase();
--- hardhat/test/SecretPhraseVerifier.ts
+++ hardhat/test/SecretPhraseVerifier.ts
@@ -1,18 +1,51 @@
 const snarkjs = require("snarkjs");
-import fs from "fs";
+const wasm_tester = require("circom_tester").wasm;
+import { computeEffEcdsaPubInput } from "@personaelabs/spartan-ecdsa";
+import path from "path";
+import { ethers } from "hardhat";
 
 describe("generate proof", () => {
-  it("should generate proof", async () => {
-    const wasm = fs.readFileSync(
-      "../circuits/SecretPhrase_js/SecretPhrase.wasm"
+  it("Generate Public inputs", async () => {
+    const [signer] = await ethers.getSigners();
+
+    const message = "hello world";
+    const signature = await signer.signMessage(message);
+    const r = BigInt(
+      `0x${Buffer.from(signature.slice(2, 66), "hex").toString("hex")}`
     );
-    const zkey = fs.readFileSync("../circuits/circuit.zkey");
-    const { proof, publicSignals } = await snarkjs.plonk.fullProve(
-      { in: 10 },
-      wasm,
-      zkey
+    const s = Buffer.from(signature.slice(66, 130), "hex");
+    const v = BigInt(
+      `0x${Buffer.from(signature.slice(130, 132), "hex").toString("hex")}`
     );
-    console.log(proof);
-    console.log(publicSignals);
+
+    const prefix = `\u0019Ethereum Signed Message:\n${message.length}`;
+    const prefixedMessage = ethers.utils.concat([
+      ethers.utils.toUtf8Bytes(prefix),
+      ethers.utils.toUtf8Bytes(message),
+    ]);
+    const msgHash = Buffer.from(
+      ethers.utils.keccak256(prefixedMessage).slice(2),
+      "hex"
+    );
+
+    const circuitPubInput = computeEffEcdsaPubInput(r, v, msgHash);
+
+    const circuitInput = {
+      s: BigInt("0x" + s.toString("hex")),
+      Tx: circuitPubInput.Tx,
+      Ty: circuitPubInput.Ty,
+      Ux: circuitPubInput.Ux,
+      Uy: circuitPubInput.Uy,
+    };
+
+    const circuit = await wasm_tester(
+      path.join(__dirname, "../circuits/SecretPhrase.circom"),
+      {
+        prime: "secq256k1",
+      }
+    );
+
+    const w = await circuit.calculateWitness(circuitInput, true);
+    await circuit.assertOut(w, { addr: BigInt(signer.address).toString(10) });
   });
 });
