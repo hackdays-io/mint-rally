7a23e67df14ba906087d912fa45014138591653a
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-07-10 21:45:13 +0900

    refactor token share

--- frontend/src/components/molecules/user/TokenModal.tsx
+++ frontend/src/components/molecules/user/TokenModal.tsx
@@ -1,14 +1,4 @@
-import {
-  Box,
-  Flex,
-  Divider,
-  Image,
-  Link,
-  Text,
-  Textarea,
-  Icon,
-  Button,
-} from "@chakra-ui/react";
+import { Box, Divider, Image, Link, Text } from "@chakra-ui/react";
 import { useAddress } from "@thirdweb-dev/react";
 import { useRouter } from "next/router";
 import { FC } from "react";
@@ -16,26 +6,17 @@ import { ipfs2http } from "../../../../utils/ipfs2http";
 import ModalBase from "../common/ModalBase";
 import { NFT } from "types/NFT";
 import { OpenseaIcon } from "../../atoms/icons/opensea/OpenseaIcon";
-import { TwitterShareButton, TwitterIcon } from "next-share";
-import { CopyIcon } from "@chakra-ui/icons";
+import { ShareButtons } from "src/components/atoms/nft/ShareButtons";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
   tokenId: number;
   nft: NFT.Metadata;
-  shareURL?: boolean;
   address: string;
 };
 
-const TokenModal: FC<Props> = ({
-  isOpen,
-  onClose,
-  nft,
-  shareURL,
-  tokenId,
-  address,
-}) => {
+const TokenModal: FC<Props> = ({ isOpen, onClose, nft, tokenId, address }) => {
   const { address: urladdress } = useRouter().query;
   const myaddress = useAddress();
 
@@ -46,11 +27,6 @@ const TokenModal: FC<Props> = ({
       address = String(urladdress);
     }
   }
-  const copyClipBoard = () => {
-    const copyText: any = document.getElementById("shareURL");
-    copyText?.select();
-    navigator.clipboard.writeText(copyText.value);
-  };
 
   const openseaLinkByChainId = () => {
     const chainId = process.env.NEXT_PUBLIC_CHAIN_ID!;
@@ -90,57 +66,21 @@ const TokenModal: FC<Props> = ({
             {tokenId}
           </Text>
 
-          {shareURL && (
-            <Box>
-              <Divider my={3} />
+          <Divider my={5} />
 
-              <Flex alignItems="center" justifyContent="center">
-                <Text mb={1}>Share on</Text>
-                <Box p={1}>
-                  <TwitterShareButton
-                    url={`https://mintrally.xyz/users/${address}?tokenid=${tokenId}`}
-                    title={`Check out my NFT on Mintrally!`}
-                    hashtags={["MintRally"]}
-                  >
-                    <TwitterIcon size={32} round />
-                  </TwitterShareButton>
-                </Box>
-                <Textarea
-                  id="shareURL"
-                  color="mint.primary"
-                  hidden={true}
-                  rows={2}
-                  p={1}
-                  value={`https://mintrally.xyz/users/${address}?tokenid=${tokenId}`}
-                ></Textarea>
-                <Icon
-                  as={CopyIcon}
-                  onClick={() => copyClipBoard()}
-                  w={6}
-                  h={6}
-                  color="green.500"
-                  size="lg"
-                  m={1}
-                ></Icon>
-              </Flex>
-            </Box>
-          )}
-          <Box>
+          <ShareButtons tokenId={tokenId} address={address} twitter={true} />
+
+          <Box mt={2}>
             <Link
               href={`${openseaLinkByChainId()}/${
                 process.env.NEXT_PUBLIC_CONTRACT_MINT_NFT_MANAGER
               }/${tokenId}`}
               target="_blank"
             >
-              <Button
-                size="small"
-                p={2}
-                width="full"
-                mt={3}
-                leftIcon={<OpenseaIcon />}
-              >
-                OpenSea
-              </Button>
+              View on OpenSea
+              <Box as="span" ml={2}>
+                <OpenseaIcon />
+              </Box>
             </Link>
           </Box>
         </Box>
