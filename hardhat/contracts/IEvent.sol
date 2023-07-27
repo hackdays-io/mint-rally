5fa565dc53fb7931b3a5c3eb18b1261e26f96f1b
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-07-27 20:07:12 +0900

    upgrade script

--- hardhat/contracts/IEvent.sol
+++ hardhat/contracts/IEvent.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.4;
 
 interface IEventManager {
-    function getIsMintLocked(uint256 _eventId) external view returns (bool);
-
     function isGroupOwnerByEventId(
         address _addr,
         uint256 _eventId
--- hardhat/scripts/upgrade_nft_stg.ts
+++ hardhat/scripts/upgrade_nft_stg.ts
@@ -2,7 +2,10 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const MintNFTFactory = await ethers.getContractFactory("MintNFT");
-  const deployedMintNFT: any = await upgrades.upgradeProxy("0xC3894D90dF7EFCAe8CF34e300CF60FF29Db9a868", MintNFTFactory);
+  const deployedMintNFT: any = await upgrades.upgradeProxy(
+    "0xC3894D90dF7EFCAe8CF34e300CF60FF29Db9a868",
+    MintNFTFactory
+  );
   await deployedMintNFT.deployed();
 
   console.log("mintNFT address:", deployedMintNFT.address);
