c1317fa7bbb5fbde9788189ce9d1dbd944cb4b76
Hal Seki <hal@plants-web.jp>
2023-08-12 15:18:04 +0900

    close #312 add Twitter OGP information

--- frontend/src/pages/users/[address].tsx
+++ frontend/src/pages/users/[address].tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Text, Container, Spinner } from "@chakra-ui/react";
+import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import { FC } from "react";
 import { GroupedCollection } from "src/components/organisms/nft/GroupedCollection";
@@ -6,13 +7,35 @@ import {
   useGetOwnedNFTByAddress,
   useSortNFTsByGroup,
 } from "src/hooks/useMintNFT";
+import { ipfs2http } from "utils/ipfs2http";
 
 const Entity: FC<{ address: string }> = ({ address }) => {
   const { nfts, isLoading } = useGetOwnedNFTByAddress(address);
   const groupedNFTs = useSortNFTsByGroup(nfts);
+  const { tokenid } = useRouter().query;
+  // if groupedNFT has tokenid, get the nft
+  const tokenId = Number(tokenid);
+  const nft = nfts.find((nft) => nft.tokenId === tokenId);
 
   return (
     <Container maxW="1000">
+      {nft && (
+        <NextSeo
+          title={`${nft.name} | MintRally`}
+          description={nft.description}
+          openGraph={{
+            url: `https://mintrally.xyz/users/${address}?tokenid=${tokenId}`,
+            images: [
+              {
+                url: ipfs2http(nft.image),
+                width: 600,
+                height: 600,
+                alt: nft.name,
+              },
+            ],
+          }}
+        ></NextSeo>
+      )}
       <Box mt={10}>
         <Heading as="h1" size="xl" color="mint.primary" fontWeight={700}>
           NFT Collection
