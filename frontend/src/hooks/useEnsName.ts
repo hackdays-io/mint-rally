2d0f68425bcd84625461cbbb82458350b4e2a6df
Hal Seki <hal@plants-web.jp>
2023-08-15 22:21:39 +0900

    implement ENS name lookup

--- frontend/src/hooks/useEnsName.ts
+++ frontend/src/hooks/useEnsName.ts
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 export const useEnsName = (address: string) => {
   const [ensName, setEnsName] = useState<string | null>(address);
-  // @todo - implement ENS lookup
   // get the ENS name from addresss using ethers.js
-
+  useEffect(() => {
+    const provider = new ethers.providers.AlchemyProvider("homestead", process.env.NEXT_PUBLIC_ALCHEMY_KEY_FOR_ENS!);
+    provider.lookupAddress(address).then((ensName) => { setEnsName(ensName); }).catch((err) => console.log(err));
+  }, [address]);
   return { ensName, setEnsName };
 };
