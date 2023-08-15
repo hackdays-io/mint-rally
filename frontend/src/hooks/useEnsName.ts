43fcb5900efe753afee9e914ea64def6e4c3c6cb
Hal Seki <hal@plants-web.jp>
2023-08-15 19:11:22 +0900

    add ens lookup todo

--- frontend/src/hooks/useEnsName.ts
+++ frontend/src/hooks/useEnsName.ts
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export const useEnsName = (address: string) => {
   const [ensName, setEnsName] = useState<string | null>(address);
+  // @todo - implement ENS lookup
+  // get the ENS name from addresss using ethers.js
 
   return { ensName, setEnsName };
 };
