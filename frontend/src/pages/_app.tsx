fecd24e2565af1d5f2287bbc5059f9ff30f0e08e
Hal Seki <hal@plants-web.jp>
2023-08-16 14:57:57 +0900

    move supportedWallet to web3Config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,24 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import {
-  ThirdwebProvider,
-  magicLink,
-  metamaskWallet,
-  safeWallet,
-} from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import Layout from "../components/layout";
 import { chakraTheme } from "../../utils/chakra-theme";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
-import { Localhost, Mumbai, Polygon } from "@thirdweb-dev/chains";
-import { useLocale } from "src/hooks/useLocale";
-import { useMemo } from "react";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import { Router } from "next/router";
-import { chainId, activeChain, useMagicLinkConfig } from "../libs/web3Config";
+import { activeChain, useMagicLinkConfig } from "../libs/web3Config";
 
 config.autoAddCss = false;
 
@@ -26,13 +18,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   Router.events.on("routeChangeStart", () => NProgress.start());
   Router.events.on("routeChangeComplete", () => NProgress.done());
   Router.events.on("routeChangeError", () => NProgress.done());
-  const { magicLinkConfig } = useMagicLinkConfig();
+  const { magicLinkConfig, supportedWallets } = useMagicLinkConfig();
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <ThirdwebProvider
         activeChain={activeChain}
-        supportedWallets={[metamaskWallet(), safeWallet(), magicLinkConfig]}
+        supportedWallets={supportedWallets}
       >
         <ChakraProvider theme={chakraTheme}>
           <Layout>
