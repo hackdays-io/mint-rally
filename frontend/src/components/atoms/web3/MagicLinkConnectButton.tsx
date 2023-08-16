b254425b8046aa6b3728651fc6d07718e1c12790
Hal Seki <hal@plants-web.jp>
2023-08-16 19:07:30 +0900

    add  email error handling and adjust for responsive design

--- frontend/src/components/atoms/web3/MagicLinkConnectButton.tsx
+++ frontend/src/components/atoms/web3/MagicLinkConnectButton.tsx
@@ -1,4 +1,6 @@
-import { Button, HStack, Img, Input } from "@chakra-ui/react";
+import { Button, HStack, Img, Input, Spacer, Stack } from "@chakra-ui/react";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useConnect } from "@thirdweb-dev/react";
 import { FC, useState } from "react";
 import { useLocale } from "src/hooks/useLocale";
@@ -14,13 +16,18 @@ const MagicLinkConnectButton: FC<Props> = ({ selected }) => {
   const connect = useConnect();
   const [isSelected, setSelected] = useState<boolean>(false);
   const [isLoading, setLoading] = useState<boolean>(false);
+  const [isNotValid, setIsNotValid] = useState<boolean>(true);
   const [enteredEmailAddress, setEnteredEmailAddress] = useState("");
   const handleConnect = async () => {
     setLoading(true);
-    await connect(magicLinkConfig, {
-      email: enteredEmailAddress,
-      chainId: Number(chainId),
-    });
+    try {
+      await connect(magicLinkConfig, {
+        email: enteredEmailAddress,
+        chainId: Number(chainId),
+      });
+    } catch (e) {
+      console.log(e);
+    }
     setLoading(false);
   };
   const EmailIcon = () => (
@@ -47,18 +54,36 @@ const MagicLinkConnectButton: FC<Props> = ({ selected }) => {
       ) : (
         <>
           <label>{t.PLEASE_ENTER_EMAIL_ADDRESS}</label>
-          <HStack>
-            <Input
-              variant="outline"
-              type="text"
-              backgroundColor={"#fff"}
-              onChange={(e) => setEnteredEmailAddress(e.target.value)}
-              pr={10}
-              placeholder={t.EMAIL_ADDRESS}
-              disabled={isLoading}
-            />
+          <Stack
+            width="100%"
+            direction={["column", "row"]}
+            align={"center"}
+            alignContent={"center"}
+            justify={"center"}
+          >
+            <HStack>
+              <Button
+                leftIcon={<FontAwesomeIcon icon={faArrowLeft} />}
+                onClick={() => {
+                  selected(false);
+                  setSelected(false);
+                }}
+              />
+              <Input
+                variant="outline"
+                type="text"
+                backgroundColor={"#fff"}
+                onChange={(e) => {
+                  setEnteredEmailAddress(e.target.value);
+                  setIsNotValid(!e.target.value.includes("@"));
+                }}
+                pr={10}
+                placeholder={t.EMAIL_ADDRESS}
+                disabled={isLoading}
+              />
+            </HStack>
             <Button
-              w={260}
+              w={160}
               style={{
                 fontWeight: "bold",
                 backgroundColor: "#562406",
@@ -69,10 +94,11 @@ const MagicLinkConnectButton: FC<Props> = ({ selected }) => {
               }}
               loadingText="Loading..."
               isLoading={isLoading}
+              disabled={isNotValid}
             >
               {t.CONNECT}
             </Button>{" "}
-          </HStack>
+          </Stack>
         </>
       )}
     </>
