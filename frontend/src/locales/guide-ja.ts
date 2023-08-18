0d395b3671d5ae123ddeb1d7d7f282dcba8f5fb1
Hal Seki <hal@plants-web.jp>
2023-08-18 14:03:54 +0900

    add English mint guide

---
+++ frontend/src/locales/guide-ja.ts
@@ -0,0 +1,19 @@
+export const guide =
+  [`MetaMaskの利用が初めての方は
+<a
+  href="https://hackdays.notion.site/Way-to-Mint-9583d30c43e6441b877128e39804253a#3d86c1130e78407292a04d4647da17df"
+  target="help"
+  style={{ textDecoration: "underline" }}
+>
+  こちら
+</a>
+をクリック。`,
+    `あいことばが分からない方はイベント主催者に確認してください。`,
+    ` MintRally についてのよくある質問は
+<a
+  href="https://hackdays.notion.site/WIP-d9872519b9254933b641a3428c697192"
+  target="help"
+  style={{ textDecoration: "underline" }}
+>
+  こちら
+</a>`]
