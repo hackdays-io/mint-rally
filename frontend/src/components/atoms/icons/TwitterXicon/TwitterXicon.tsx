17a9b1a8e646b98702f6fa4bc692c40c290e14eb
LurieK <LuraineKimmerle@gmail.com>
2023-08-26 13:19:46 +0800

    rename const

--- frontend/src/components/atoms/icons/TwitterXicon/TwitterXicon.tsx
+++ frontend/src/components/atoms/icons/TwitterXicon/TwitterXicon.tsx
@@ -1,7 +1,7 @@
 import { Icon } from '@chakra-ui/react';
 import { FC } from 'react';
 
-export const NewIcon: FC = () => (
+export const XIcon: FC = () => (
   <Icon height="100%" width="100%" maxWidth="20px">
     <svg height="100%" width="100%" viewBox="0 0 1668.56 1221.19" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M485.39,356.79l230.07,307.62L483.94,914.52h52.11l202.7-218.98l163.77,218.98h177.32
