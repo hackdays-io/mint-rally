1ada772ad796885690f8100b0b59c2f629639a91
LurieK <LuraineKimmerle@gmail.com>
2023-08-26 13:19:20 +0800

    create twitterX component

---
+++ frontend/src/components/atoms/icons/TwitterXicon/TwitterXicon.tsx
@@ -0,0 +1,12 @@
+import { Icon } from '@chakra-ui/react';
+import { FC } from 'react';
+
+export const NewIcon: FC = () => (
+  <Icon height="100%" width="100%" maxWidth="20px">
+    <svg height="100%" width="100%" viewBox="0 0 1668.56 1221.19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M485.39,356.79l230.07,307.62L483.94,914.52h52.11l202.7-218.98l163.77,218.98h177.32
+        L836.82,589.6l215.5-232.81h-52.11L813.54,558.46L662.71,356.79H485.39z M562.02,395.17h81.46l359.72,480.97h-81.46L562.02,395.17
+        z" fill="white" />
+    </svg>
+  </Icon>
+);
