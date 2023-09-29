0d9005a3ca73096c03d63aa0678fe05aef9749c7
yu23ki14 <yuki_021423@yahoo.co.jp>
2023-09-29 16:31:58 +0900

    modify deploy script

--- hardhat/scripts/upgrades/v1.3.0/upgrade_prd.ts
+++ hardhat/scripts/upgrades/v1.3.0/upgrade_prd.ts
@@ -1,24 +1,23 @@
-import { ethers, upgrades, network } from "hardhat";
+import { network } from "hardhat";
+import { upgradeEventManager, upgradeMintNFT } from "../../helper/upgrade";
+import { deployOperationController } from "../../helper/deploy";
 
 async function main() {
   if (network.name !== "polygon") throw new Error("wrong network");
 
-  const MintNFTFactory = await ethers.getContractFactory("MintNFT");
-  const deployedMintNFT: any = await upgrades.upgradeProxy(
-    process.env.POLYGON_MINTNFT_ADDRESS!,
-    MintNFTFactory
-  );
-  await deployedMintNFT.deployed();
+  const deployedOperationController = await deployOperationController();
 
-  // // const EventManagerFactory = await ethers.getContractFactory("EventManager");
-  // // const deployedEventManager: any = await upgrades.upgradeProxy(
-  // //   process.env.POLYGON_EVENTMANAGER_ADDRESS!,
-  // //   EventManagerFactory
-  // // );
-  // // await deployedEventManager.deployed();
-
-  console.log("mintNFT address:", deployedMintNFT.address);
-  // // console.log("eventManager address:", deployedEventManager.address);
+  await upgradeMintNFT([
+    process.env.POLYGON_FOWARDER_ADDRESS,
+    process.env.POLYGON_SECRETPHRASE_VERIFIER_ADDRESS,
+    deployedOperationController.address,
+  ]);
+  await upgradeEventManager([
+    process.env.POLYGON_RELAYER_ADDRESS,
+    250000,
+    1000000,
+    deployedOperationController.address,
+  ]);
 }
 
 main().catch((error) => {
