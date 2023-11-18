3195eaa7ec591c97b383b73fbc65064a1897da04
Tanapol Hongsuwan <tawan11432@gmail.com>
2023-11-18 16:49:17 +0900

    added footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -52,6 +52,54 @@ const Footer = () => (
           X
         </Box>
       </Link>
+      <Link href="https://hackdays.notion.site/86dea1026e4943c180f806027d200815?pvs=4" target="_blank">
+        <Box
+          cursor="pointer"
+          as="span"
+          py="1"
+          px="3"
+          ml={2}
+          backgroundColor="mint.bg"
+          borderRadius="full"
+          fontSize="sm"
+          color="mint.primary"
+          fontWeight="bold"
+        >
+          Terms
+        </Box>
+      </Link>
+      <Link href="https://hackdays.notion.site/d13993b3865344d6b6d754821b057a10?pvs=4" target="_blank">
+        <Box
+          cursor="pointer"
+          as="span"
+          py="1"
+          px="3"
+          ml={2}
+          backgroundColor="mint.bg"
+          borderRadius="full"
+          fontSize="sm"
+          color="mint.primary"
+          fontWeight="bold"
+        >
+          Privacy
+        </Box>
+      </Link>
+      <Link href="https://hackdays.notion.site/1bccde1919f04d78a5b0d5b148c65f11?pvs=4" target="_blank">
+        <Box
+          cursor="pointer"
+          as="span"
+          py="1"
+          px="3"
+          ml={2}
+          backgroundColor="mint.bg"
+          borderRadius="full"
+          fontSize="sm"
+          color="mint.primary"
+          fontWeight="bold"
+        >
+          SCTA
+        </Box>
+      </Link>
     </Box>
   </Box>
 );
